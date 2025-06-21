021f8baec40e38969916143f2d5ebc5eb83d25c0
Gema Dodi Pranata <gemadp01@gmail.com>
2025-06-21 21:26:30 +0700

    feat: component - declarative rendering dan event handling (materi bind)

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,7 +1,16 @@
 class Button {
+  constructor() {
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log("clicked");
+  }
+
   render() {
-    return `
-			<button>Click me</button>
-		`;
+    const button = document.createElement(`button`);
+    button.innerText = "Click me";
+    button.addEventListener("click", this.handleClick);
+    return button;
   }
 }
