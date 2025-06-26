e3c7c2d1f81cba436dab414f6cd2a4230387b1b6
Gema Dodi Pranata <gemadp01@gmail.com>
2025-06-26 15:27:25 +0700

    konfigurasi router dasar (hash) menjadi lebih modular

--- utils/route.js
+++ utils/route.js
@@ -9,14 +9,24 @@ const ROUTES = {
 export const route = (hash) => {
   const appContainer = document.getElementById("app");
   appContainer.innerHTML = "";
-  if (hash === "") {
-    // berada di homepage
-    appContainer.appendChild(ROUTES.home);
-  } else if (hash === "detail") {
-    // berada di detail
-    appContainer.appendChild(ROUTES.detail);
+
+  const hashRoute = hash === "" ? "home" : hash;
+  const isAvailablePage = Object.keys(ROUTES).includes(hashRoute);
+
+  if (isAvailablePage) {
+    appContainer.appendChild(ROUTES[hashRoute]);
   } else {
-    // berada di halaman 404 (not found)
     alert("page not found");
   }
+
+  // if (hash === "") {
+  //   // berada di homepage
+  //   appContainer.appendChild(ROUTES.home);
+  // } else if (hash === "detail") {
+  //   // berada di detail
+  //   appContainer.appendChild(ROUTES.detail);
+  // } else {
+  //   // berada di halaman 404 (not found)
+  //   alert("page not found");
+  // }
 };
